4f6e39ab69379edb7d520b5c50019d2ec0393065
TPabasara <ptharindu5287@gmail.com>
2025-05-15 19:20:40 +0530

    updated code

--- back/app/routers/database/database.py
+++ back/app/routers/database/database.py
@@ -4,8 +4,6 @@ from sqlalchemy.orm import sessionmaker
 
 from .config import DATABASE_URL  #this occur an error need to solve.
 
-#SQLALCHEMY_DATABASE_URL = 'postgresql://e-commerce_owner:npg_h1XZRplf0WdH@ep-long-lab-a8pgrxso-pooler.eastus2.azure.neon.tech/e-commerce?sslmode=require'
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
